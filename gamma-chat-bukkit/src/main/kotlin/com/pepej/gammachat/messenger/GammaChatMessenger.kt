e9765b9571a98d2ed7ac8f2a0b6e314a99a96b4f
pepej <gamzan127@gmail.com>
2021-10-10 15:38:54 +1000

    Initial commit

--- gamma-chat-bukkit/src/main/kotlin/com/pepej/gammachat/messenger/GammaChatMessenger.kt
+++
@@ -1,7 +0,0 @@
-package com.pepej.gammachat.messenger
-
-import com.pepej.papi.messaging.InstanceData
-import com.pepej.papi.messaging.Messenger
-import org.bukkit.plugin.messaging.PluginMessageListener
-
-interface GammaChatMessenger : Messenger, InstanceData, PluginMessageListener
